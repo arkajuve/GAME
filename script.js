a49b8522f3308022dd2c0ce18fbd7cb0401ed7e3
arkajuve <chiara.manni@bbz-cfp.ch>
2022-12-10 00:06:12 +0100

    version friday

--- script.js
+++ script.js
@@ -10,67 +10,67 @@ var spielfeld = document.querySelector(".playground");
 var backgroundPosition = 0;
 
 var sprung = 0;
+let jump = false;
 
 function loop() {
-  backgroundPosition = backgroundPosition + 5;
+  //background
+  backgroundPosition = backgroundPosition + 6;
   spielfeld.style.backgroundPosition = `-${backgroundPosition}px 0`;
 
-  if (parseInt(spieler.style.top) < 200) {
+  //spieler
+  if (parseInt(spieler.style.top) < 50) {
     spieler.style.top = parseInt(spieler.style.top) + 5 + "px";
   }
 
+  //pilz timer
   if (timer.ready()) {
     var h = document.createElement("div");
-    h.classList.add("stein");
-    h.style.top = "400px";
+    h.classList.add("pilz");
+    h.style.bottom = "200px";
     h.style.right = "0px";
     spielfeld.appendChild(h);
   }
 
-  var steine = document.querySelectorAll(".stein");
-  for (var stein of steine) {
-    stein.style.right = parseInt(stein.style.right) + 5 + "px";
-    if (parseInt(stein.style.right) > 1180) {
-      stein.parentNode.removeChild(stein);
+  //pilze
+  var pilze = document.querySelectorAll(".pilz");
+  for (var pilz of pilze) {
+    pilz.style.right = parseInt(pilz.style.right) + 6 + "px";
+    if (parseInt(pilz.style.right) > 1180) {
+      pilz.parentNode.removeChild(pilz);
     }
   }
 
+  //score
   if (parseInt(spieler.style.top) > 0) {
     score = score + 1;
     punkteAnzeige.textContent = score;
   }
 
-  if (keyboard(32)) {
-    spieler.style.top = parseInt(spieler.style.top) - 11 + "px";
-  }
-
-  if (anyCollision(spieler, steine)) {
-    alert("Game over!");
+  //collision
+  if (anyCollision(spieler, pilze)) {
     location.href = "gameover.html";
     return;
   }
 
   //jump
   spieler.style.top = parseInt(spieler.style.top) - sprung + "px";
+  if (keyboard(32)) {
+    spieler.style.top = parseInt(spieler.style.top) - 11 + "px";
+    jump = true;
+  }
 
-  //sprung
-  if (parseInt(spieler.style.top) < 390) {
+  if (parseInt(spieler.style.top) < 310) {
     sprung = sprung - 0.1;
   } else {
     sprung = 0;
-    spieler.style.top = "390px";
-  }
-  if (keyboard(32) && parseInt(spieler.style.top) >= 390) {
-    sprung = 4;
-  }
-  //Keyboard jump
-  if (keyboard(38)) {
-    spieler.style.top = parseInt(spieler.style.top) - 11 + "px";
+    spieler.style.top = "310px";
+    jump = false;
   }
 
-  //anziehungskraft
-  if (parseInt(spieler.style.top) < 450) {
-    spieler.style.top = parseInt(spieler.style.top) + 4 + "px";
+  if (jump) {
+    spieler.style.backgroundImage = "url(wurm_jump.png)";
+  } else {
+    spieler.style.backgroundImage = "url(wurm.gif)";
   }
 
   window.requestAnimationFrame(loop);
