54fc4b7226dda817be0ce33d0c30adb76155c666
arkajuve <chiara.manni@bbz-cfp.ch>
2022-12-16 16:18:15 +0100

    finish friday

--- script.js
+++ script.js
@@ -1,65 +1,20 @@
-var spieler = document.querySelector(".player");
+let spieler = document.querySelector(".player");
 spieler.style.left = "300px";
 
-var punkteAnzeige = document.querySelector(".punkte");
-var score = 0;
+let punkteAnzeige = document.querySelector(".punkte");
+let score = 0;
 
-var timer = new Timer(90);
+let timer = new Timer(70);
 
-var spielfeld = document.querySelector(".playground");
-var backgroundPosition = 0;
+let spielfeld = document.querySelector(".playground");
+let backgroundPosition = 0;
 
-var sprung = 0;
+let sprung = 0;
 let jump = false;
 
-var jumpsound = new Audio("jump.mp3");
+let jumpsound = new Audio("jump.mp3");
 
-function loop() {
-  //background bewegung
-  backgroundPosition = backgroundPosition + 6;
-  spielfeld.style.backgroundPosition = `-${backgroundPosition}px 0`;
-
-  //spieler
-  if (parseInt(spieler.style.top) < 80) {
-    spieler.style.top = parseInt(spieler.style.top) + 5 + "px";
-  }
-
-  //pilz timer
-  if (timer.ready()) {
-    var h = document.createElement("div");
-    h.classList.add("pilz");
-    h.style.bottom = "200px";
-    h.style.right = "-10px";
-    spielfeld.appendChild(h);
-  }
-
-  //pilze
-  //wie schnell die pilze nacheinander einblenden
-  var pilze = document.querySelectorAll(".pilz");
-  for (var pilz of pilze) {
-    pilz.style.right = parseInt(pilz.style.right) + 6 + "px";
-    if (parseInt(pilz.style.right) > 1110) {
-      pilz.parentNode.removeChild(pilz);
-    }
-  }
-
-  //score
-  if (parseInt(spieler.style.top) > 0) {
-    score = score + 10;
-    punkteAnzeige.textContent = score;
-  }
-
-  //sound beim springen
-  if (keyboard(32)) {
-    jumpsound.play();
-  }
-
-  //collision
-  //wenn kollision passiert, dann auf gameoverseite geschickt
-  if (anyCollision(spieler, pilze)) {
-    location.href = "gameover.html";
-  }
-
+function jumpfunction() {
   //jump
   spieler.style.top = parseInt(spieler.style.top) - sprung + "px";
   if (keyboard(32)) {
@@ -81,8 +36,64 @@ function loop() {
   } else {
     spieler.style.backgroundImage = "url(wurm.gif)";
   }
+}
+
+function pilztimer() {
+  //pilz timer
+  if (timer.ready() && Math.random() > 0.4) {
+    let h = document.createElement("div");
+    h.classList.add("pilz");
+    h.style.bottom = "200px";
+    h.style.right = "-10px";
+    spielfeld.appendChild(h);
+  }
+}
+
+function pilzfunction() {
+  //pilze
+  //wie schnell die pilze nacheinander einblenden
+  let pilze = document.querySelectorAll(".pilz");
+  for (let pilz of pilze) {
+    pilz.style.right = parseInt(pilz.style.right) + 6 + "px";
+    if (parseInt(pilz.style.right) > 1110) {
+      pilz.parentNode.removeChild(pilz);
+    }
+  }
+
+  pilztimer();
+
+  //collision
+  //wenn kollision passiert, dann auf gameoverseite geschickt
+  if (anyCollision(spieler, pilze)) {
+    location.href = "gameover.html";
+  }
+}
+
+function loop() {
+  //background bewegung
+  backgroundPosition = backgroundPosition + 6;
+  spielfeld.style.backgroundPosition = `-${backgroundPosition}px 0`;
+
+  //spieler
+  if (parseInt(spieler.style.top) < 80) {
+    spieler.style.top = parseInt(spieler.style.top) + 5 + "px";
+  }
+
+  pilzfunction();
+
+  //score
+  if (parseInt(spieler.style.top) > 0) {
+    score = score + 10;
+    punkteAnzeige.textContent = score;
+  }
+
+  //sound beim springen
+  if (keyboard(32)) {
+    jumpsound.play();
+  }
+
+  jumpfunction();
 
   window.requestAnimationFrame(loop);
 }
-
 window.requestAnimationFrame(loop);
